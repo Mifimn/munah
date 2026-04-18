eb99c859864c1fb081bd9a7520456478efe9c51e
Musa ayoola shittu <109434707+Mifimn@users.noreply.github.com>
2026-04-18 21:20:05 +0100

    Update route.ts

--- app/api/get-code/route.ts
+++ app/api/get-code/route.ts
@@ -2,12 +2,17 @@ import { NextResponse } from 'next/server';
 
 export async function GET() {
   const SHIPBUBBLE_KEY = process.env.SHIPBUBBLE_API_KEY;
-  const response = await fetch("https://api.shipbubble.com/v1/shipping/labels/categories", {
-    method: 'GET',
-    headers: {
-      'Authorization': `Bearer ${SHIPBUBBLE_KEY}`,
-      'Content-Type': 'application/json'
-    }
+  const payload = {
+    name: "Modina Natural Cure",
+    email: "shittumifimn0807@gmail.com",
+    phone: "+2348023169274",
+    address: "1, OLAGUNJU OMOTAYO CRESCENT, ATUNSE STREET, LAGOS, LAGOS STATE, NIGERIA"
+  };
+
+  const response = await fetch("https://api.shipbubble.com/v1/shipping/address/validate", {
+    method: 'POST',
+    headers: { 'Authorization': `Bearer ${SHIPBUBBLE_KEY}`, 'Content-Type': 'application/json' },
+    body: JSON.stringify(payload)
   });
   const data = await response.json();
   return NextResponse.json(data);
